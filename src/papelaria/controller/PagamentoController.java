40aa2eba18cc6065d26e17eea8579284dbb976fb
AndreGadens <andregadens1@gmail.com>
2026-06-17 13:45:49 -0300

    Update PagamentoController.java

--- src/papelaria/controller/PagamentoController.java
+++ src/papelaria/controller/PagamentoController.java
@@ -15,15 +15,14 @@ public class PagamentoController {
 
     private static final String ARQUIVO = "pagamentos.txt";
 
-    public PagamentoController() {
-
-        this.view = new PagamentoView(this);
-        carregarDoArquivo();
-    }
-
     private Map<Integer, Pagamento> pagamentos = new HashMap<>();
     private int proximoId = 1;
     private PagamentoView view;
+
+    public PagamentoController() {
+        this.view = new PagamentoView(this);
+        carregarDoArquivo();
+    }
 
     public void iniciar() {
 
@@ -69,14 +68,10 @@ public class PagamentoController {
     public void registrarPagamento(double valor, String status, String chavePix) {
 
         if(valor <= 0){
-            throw new IllegalArgumentException(
-                    "Valor deve ser maior que zero");
+            throw new IllegalArgumentException("Valor deve ser maior que zero");
         }
 
-        Pagamento pagamento = new PagamentoPix(proximoId,
-                valor,
-                status,
-                chavePix);
+        Pagamento pagamento = new PagamentoPix(proximoId, valor, status, chavePix);
 
         pagamentos.put(proximoId, pagamento);
 
@@ -93,11 +88,7 @@ public class PagamentoController {
                     "Valor deve ser maior que zero");
         }
 
-        Pagamento pagamento = new PagamentoCartao(
-                proximoId,
-                valor,
-                status,
-                parcelas);
+        Pagamento pagamento = new PagamentoCartao(proximoId, valor, status, parcelas);
 
         pagamentos.put(proximoId, pagamento);
 
@@ -141,21 +132,12 @@ public class PagamentoController {
             if(pagamento instanceof PagamentoPix) {
 
                 PagamentoPix pix = (PagamentoPix) pagamento;
-                linhas.add("PIX;" +
-                        pix.getId() + ";" +
-                        pix.getValor() + ";" +
-                        pix.getStatus() + ";" +
-                        pix.getChavePix()
-                );
+                linhas.add("PIX;" + pix.getId() + ";" + pix.getValor() + ";" + pix.getStatus() + ";" + pix.getChavePix());
             }
             else if(pagamento instanceof PagamentoCartao) {
 
                 PagamentoCartao cartao = (PagamentoCartao) pagamento;
-                linhas.add("CARTAO;" +
-                        cartao.getId() + ";" +
-                        cartao.getValor() + ";" +
-                        cartao.getStatus() + ";" +
-                        cartao.getParcelas()
+                linhas.add("CARTAO;" + cartao.getId() + ";" + cartao.getValor() + ";" + cartao.getStatus() + ";" + cartao.getParcelas()
                 );
             }
         }
@@ -175,12 +157,7 @@ public class PagamentoController {
                 String status = partes[3];
                 String chavePix = partes[4];
 
-                Pagamento pagamento =
-                        new PagamentoPix(
-                                id,
-                                valor,
-                                status,
-                                chavePix);
+                Pagamento pagamento = new PagamentoPix(id, valor, status, chavePix);
 
                 pagamentos.put(id, pagamento);
             }
@@ -190,23 +167,17 @@ public class PagamentoController {
                 String status = partes[3];
                 int parcelas = Integer.parseInt(partes[4]);
 
-                Pagamento pagamento =
-                        new PagamentoCartao(
-                                id,
-                                valor,
-                                status,
-                                parcelas);
+                Pagamento pagamento = new PagamentoCartao(id, valor, status, parcelas);
 
                 pagamentos.put(id, pagamento);
             }
         }
         if(!pagamentos.isEmpty()) {
 
-            proximoId =
-                    pagamentos.keySet()
-                            .stream()
-                            .max(Integer::compareTo)
-                            .get() + 1;
+            proximoId = pagamentos.keySet()
+                    .stream()
+                    .max(Integer::compareTo)
+                    .get() + 1;
         }
     }
 }
